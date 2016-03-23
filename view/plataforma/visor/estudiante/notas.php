9ff927628f997987df942599f21d892c0e6c9795
FER <megatroninlove@gmail.com>
2016-03-23 01:58:50 -0500

    cambios / materias

--- view/plataforma/visor/estudiante/notas.php
+++ view/plataforma/visor/estudiante/notas.php
@@ -1,41 +1,113 @@
-<div class="contEstandar bWhite rr5 p0 ng-scope">
-    
-
-    <div class="col-sm-4 col-xs-4 bBlueL rr5 pInt colorVirde">
+<div class="contEstandar bWhite rr5 p1025">
 
 
 
+<div class="table-responsive">
+            <table class="table table-hover">
+                <thead>
+                  <tr>
+                    <th class="fv_fn"><span>Materia</span></th>
+                    <th>(1) PE</th>
+                    <th>(2) PE</th>
+                    <th>(3) PE</th>
+                    <th>EF</th>
+                    <th>NF</th>
+                  </tr>
+                </thead>
+                <tbody>
+                                       
+                      <tr>
+                      	<td class="bWhite"><div class="dIB bMat p5 rr50"></div> &nbsp; <b>Matemáticas</b></td>
+                        <td class="bWhite"><div class="taR vcenter colorVerde ff2 t20 fv_pir1"><span>▲</span> 3.8</div></td>
+                        <td class="bWhite">&nbsp;</td>
+                        <td class="bWhite">&nbsp;</td>
+                        <td class="bWhite">&nbsp;</td>
+                        <td class="bWhite"><div class="taR vcenter colorVerde ff2 t20">3.8</div></td>
+                      </tr>
+                      <tr>
+                        <td class="bWhite"><div class="dIB bLyl p5 rr50"></div> &nbsp; <b>Lengua y Literatura</b></td>
+                        <td class="bWhite"><div class="taR vcenter colorRojo ff2 t20 fv_pir1"><span>▼</span> 3.8</div></td>
+                        <td class="bWhite">&nbsp;</td>
+                        <td class="bWhite">&nbsp;</td>
+                        <td class="bWhite">&nbsp;</td>
+                        <td class="bWhite"><div class="taR vcenter colorRojo ff2 t20">3.8</div></td>
+                      </tr>
+                      <tr>
+                        <td class="bWhite"><div class="dIB bBio p5 rr50"></div> &nbsp; <b>Biología</b></td>
+                        <td class="bWhite"><div class="taR vcenter colorVerde ff2 t20 fv_pir1"><span>▼</span> 4.3</div></td>
+                        <td class="bWhite">&nbsp;</td>
+                        <td class="bWhite">&nbsp;</td>
+                        <td class="bWhite">&nbsp;</td>
+                        <td class="bWhite"><div class="taR vcenter colorVerde ff2 t20">4.3</div></td>
+                      </tr>
+                      <tr>
+                        <td class="bWhite"><div class="dIB bSoc p5 rr50"></div> &nbsp; <b>Sociales</b></td>
+                        <td class="bWhite"><div class="taR vcenter colorVerde ff2 t20 fv_pir1"><span>▲</span> 3.5</div></td>
+                        <td class="bWhite">&nbsp;</td>
+                        <td class="bWhite">&nbsp;</td>
+                        <td class="bWhite">&nbsp;</td>
+                        <td class="bWhite"><div class="taR vcenter colorVerde ff2 t20">3.5</div></td>
+                      </tr>
+                      <tr>
+                        <td class="bWhite"><div class="dIB bLex p5 rr50"></div> &nbsp; <b>Lengua Extranjera</b></td>
+                        <td class="bWhite"><div class="taR vcenter colorOrange ff2 t20 fv_pir1"><span>▼</span> 3.1</div></td>
+                        <td class="bWhite">&nbsp;</td>
+                        <td class="bWhite">&nbsp;</td>
+                        <td class="bWhite">&nbsp;</td>
+                        <td class="bWhite"><div class="taR vcenter colorOrange ff2 t20">3.1</div></td>
+                      </tr>
+                      <tr>
+                        <td class="bWhite"><div class="dIB bFcv p5 rr50"></div> &nbsp; <b>Formación Cristiana y Valores</b></td>
+                        <td class="bWhite"><div class="taR vcenter colorVerde ff2 t20 fv_pir1"><span>▲</span> 3.1</div></td>
+                        <td class="bWhite">&nbsp;</td>
+                        <td class="bWhite">&nbsp;</td>
+                        <td class="bWhite">&nbsp;</td>
+                        <td class="bWhite"><div class="taR vcenter colorVerde ff2 t20">3.1</div></td>
+                      </tr>
+                      <tr>
+                        <td class="bWhite"><div class="dIB bEdf p5 rr50"></div> &nbsp; <b>Educación Física</b></td>
+                        <td class="bWhite"><div class="taR vcenter colorVerde ff2 t20 fv_pir1"><span>▲</span> 4.0</div></td>
+                        <td class="bWhite">&nbsp;</td>
+                        <td class="bWhite">&nbsp;</td>
+                        <td class="bWhite">&nbsp;</td>
+                        <td class="bWhite"><div class="taR vcenter colorVerde ff2 t20">4.0</div></td>
+                      </tr>
+                      <tr>
+                        <td class="bWhite"><div class="dIB bQui p5 rr50"></div> &nbsp; <b>Química</b></td>
+                        <td class="bWhite"><div class="taR vcenter colorRojo ff2 t20 fv_pir1"><span>▼</span>2.6</div></td>
+                        <td class="bWhite">&nbsp;</td>
+                        <td class="bWhite">&nbsp;</td>
+                        <td class="bWhite">&nbsp;</td>
+                        <td class="bWhite"><div class="taR vcenter colorRojo ff2 t20">2.6</div></td>
+                      </tr>
+                      <tr>
+                        <td class="bWhite"><div class="dIB bFil p5 rr50"></div> &nbsp; <b>Filosofía</b></td>
+                        <td class="bWhite"><div class="taR vcenter colorOrange ff2 t20 fv_pir1"><span>▲</span>2.6</div></td>
+                        <td class="bWhite">&nbsp;</td>
+                        <td class="bWhite">&nbsp;</td>
+                        <td class="bWhite">&nbsp;</td>
+                        <td class="bWhite"><div class="taR vcenter colorOrange ff2 t20">2.6</div></td>
+                      </tr>                                                                                                                                    
 
-        
-        <div class="tab-content">
-            <div id="p1" class="tab-pane fade in active">
-                <br>
-                                <div class="contBar rr20 mb10">
-                    <div class="bOrange p5 colorfff ff2" style="width:53%;">0.1</div>
-                    <span class="colorfff ff2 porceFul">53%</span>
-                </div>
-                                <div class="contBar rr20 mb10">
-                    <div class="bVerde p5 colorfff ff2" style="width:07%;">3.6</div>
-                    <span class="colorfff ff2 porceFul">07%</span>
-                </div>
-                                <div class="contBar rr20 mb10">
-                    <div class="bRed p5 colorfff ff2" style="width:37%;">2.9</div>
-                    <span class="colorfff ff2 porceFul">37%</span>
-                </div>
-                                <div class="contBar rr20 mb10">
-                    <div class="bOrange p5 colorfff ff2" style="width:66%;">1.9</div>
-                    <span class="colorfff ff2 porceFul">66%</span>
-                </div>
-                                <div class="contBar rr20 mb10">
-                    <div class="bVerde p5 colorfff ff2" style="width:40%;">2.1</div>
-                    <span class="colorfff ff2 porceFul">40%</span>
-                </div>
-            </div>
 
+                </tbody>
+            </table>
+</div>
 
-        </div>
+<div class="row bOrangeL p10 m0">
 
+<div class="col-sm-3 col-xs-3 vcenter">
+  <span class="colorVirde ff2">PE</span> &nbsp;
+  <span class="color666">Periodo</span>
+  <br>
+  <span class="colorVirde ff2">EF</span> &nbsp;
+  <span class="color666">Examen Final</span>
+  <br>
+  <span class="colorVirde ff2">NF</span> &nbsp;
+  <span class="color666">Nota Final</span>
+</div>
 
-        
-        
-    </div>
+</div>
+
+</div>
+</div>
