4ad464bae697ef7fe6841d3c9a509025b9c7ef8f
FER <megatroninlove@gmail.com>
2016-03-23 03:09:11 -0500

    cambios / materias

--- view/plataforma/visor/estudiante/notas.php
+++ view/plataforma/visor/estudiante/notas.php
@@ -1,6 +1,4 @@
-<div class="contEstandar bWhite rr5 p1025">
-
-
+<div class="contEstandar bWhite rr5 p0">
 
 <div class="table-responsive">
             <table class="table table-hover">
@@ -94,7 +92,26 @@
             </table>
 </div>
 
-<div class="row bOrangeL p10 m0">
+<div class="col-sm-4 col-xs-4 bBlueL rr5 colorVirde p20">
+  <b>ÚLTIMAS CALIFICACIONES</b>
+  <br>
+
+<div class="col-sm-4 col-xs-4 ">
+<span>Nov 12 - 15</span>
+  <div class="contEstandar rr5  bWhite">
+      <div class="colorfff bVirde dIB rr10" style="padding:2px 2px;">&nbsp; 1 &nbsp;</div>
+  <br>
+  <div class="dIB bFil p5 rr50"></div> &nbsp; <b>Filosofía</b>
+  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+  <div class="taR vcenter colorOrange ff2 t20">2.6</div>
+  </div>
+</div>
+
+</div>
+
+<br style="clear:both;">
+
+<div class="row bOrangeL m0 t12 p1020 fv_span">
 
 <div class="col-sm-3 col-xs-3 vcenter">
   <span class="colorVirde ff2">PE</span> &nbsp;
@@ -107,6 +124,24 @@
   <span class="color666">Nota Final</span>
 </div>
 
+<div class="col-sm-3 col-xs-3 vcenter">
+  <span class="colorVirde ff2">TC</span> &nbsp;
+  <span class="color666">Talleres en Casa</span>
+  <br>
+  <span class="colorVirde ff2">AS</span> &nbsp;
+  <span class="color666">Actividades en Clase</span>
+  <br>
+  <span class="colorVirde ff2">E</span> &nbsp;
+  <span class="color666">Evaluaciones</span>
+</div>
+
+<div class="col-sm-3 col-xs-3 vcenter">
+  <span class="colorVirde ff2">CP</span> &nbsp;
+  <span class="color666">Comportamiento y Participación</span>
+  <br>
+  <span class="colorVirde ff2">EF</span> &nbsp;
+  <span class="color666">Evaluación Final</span>
+</div>
 </div>
 
 </div>
