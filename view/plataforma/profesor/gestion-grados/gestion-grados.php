39cad12659df744a9a964bfbd2a6b832041102c1
FER <megatroninlove@gmail.com>
2016-03-24 17:58:44 -0500

    Fer
    plataforma/profesor/gestion-grados // ok

--- view/plataforma/profesor/gestion-grados/gestion-grados.php
+++ view/plataforma/profesor/gestion-grados/gestion-grados.php
@@ -1,5 +1,40 @@
-<a href="#/plataforma/profesor/materia">
-    <center>
-        <img src="resources/img/temporal/a1.jpg" />
-    </center>
-</a>
+<div class="fv_mt50 ff2 t20 taC fv_bleu">Gestionar mi clase por grados</div>
+<div class="p30" style="max-width:450px; margin-left:auto; margin-right:auto;">
+    <div class="row">
+        <div class="col-sm-12 col-xs-12">
+        
+            <div class="col-sm-12 col-xs-12 mb10">
+                <select class="selSt fv_fe">
+                    <option>Matemáticas</option>
+                    <option>Trigonometría</option>
+                </select>
+            </div>
+            <div class="col-sm-6 col-xs-6 mb10">
+                <select class="selSt fv_fe1">
+                    <option>Grado</option>
+                </select>
+            </div>
+            <div class="col-sm-6 col-xs-6 mb10">
+                <select class="selSt fv_fe1">
+                    <option>Grupo</option>
+                </select>
+            </div>
+            <div class="col-sm-12 col-xs-12 mb20">
+                <select class="selSt fv_fe1">
+                    <option>Jornada</option>
+                </select>
+            </div>
+            
+            <br style="clear:both;" />
+            <div class="taC fv_full">
+                <a href="#/plataforma/profesor/materia" class="bBasic5 rr40 t20 ff2 colorfff cP fv_verde1">
+                    <span class="p20">Gestionar grado</span>
+                </a>
+            </div>
+            
+        </div>
+    </div>
+
+</div>
+    
+    
