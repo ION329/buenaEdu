88e3aee2a435db4bc7120d0abd657d5182ea5f0a
FER <megatroninlove@gmail.com>
2016-03-26 02:08:08 -0500

    plataforma/profesor/perfil
    en proceso. puse imágenes en lo que falta

--- view/plataforma/profesor/perfil/perfil.php
+++ view/plataforma/profesor/perfil/perfil.php
@@ -1,1 +1,61 @@
-<img src="resources/img/temporal/2.jpg">
+<div class="ff2 tU t14">
+<div class="vcenter fv_cal">
+    <img src="resources/img/icons/adm3.png">
+</div>
+<div class="vcenter">
+<div class="btn-group" style="width: 105px;">
+    <button type="button" class="rr50 btn btn-primary bGray fv_0202" style="border-color:#ccc;">
+        <img src="resources/img/icons/izq1.png">
+    </button>
+    <button type="button" class="rr50 btn btn-primary bGray fv_0202" style="border-color:#ccc;">
+        <img src="resources/img/icons/der1.png">
+    </button>
+</div>
+</div>
+<div class="vcenter">
+    <span class="t24 colorVirde ff2">Enero</span>
+    <span class="t24 color666 ff0">2016</span>
+    <span class="t16 fv_pper"><b>24 Clases</b></span>
+</div>
+</div>
+
+<div><img src="resources/img/temporal/00profesor-resumen.png"></div>
+
+<div class="contEstandar p20">
+    <div class="fv_mL50"><b>Martes, Enero 13 / 2016</b></div>
+</div>
+
+<div class="contEstandar p0 col-sm-9 col-xs-9"><img src="resources/img/temporal/00profesor-resumen1.png"></div>
+
+<div class="contEstandar p20 col-sm-3 col-xs-3">
+<div class="p10">
+            
+<div class="fv_pre1 t24 ff2 fR">3.8</div>
+
+<div class="fv_promedionota">
+<div class="fv_promedio_t">Promedio Nota</div>
+<div class="fv_prom_cont mb5 rr0">
+<div class="fv_gris1 colorfff ff2" style="width:70%;">
+    <div class="fv_verde1 p10 colorfff ff2" style="width:40%;"></div>
+</div>
+<span class="color000 porceFul fv_pre">40%</span>
+</div>
+</div>
+
+<div class="fv_per colorVirde ff2 tU t14">        
+<div class="row">
+    <div class="col-sm-3 col-xs-3">(1)P</div>
+    <div class="col-sm-3 col-xs-3">(2)P</div>
+    <div class="col-sm-3 col-xs-3">(3)P</div>
+    <div class="col-sm-3 col-xs-3">EF</div>
+</div> 
+</div>
+<div class="row ff2 t16">
+    <div class="col-sm-3 col-xs-3 taC"><span class="ff0 t12">3.6</span></div>
+    <div class="col-sm-3 col-xs-3 taC"><span class="ff0 t12">4.2</span></div>
+    <div class="col-sm-3 col-xs-3 taC"><img src="resources/img/icons/block.png"></div>
+    <div class="col-sm-3 col-xs-3 taC"><img src="resources/img/icons/block.png"></div>
+</div>
+
+</div>
+</div>
