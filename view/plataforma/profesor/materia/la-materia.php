3cacbe5498f07cd37fa01e52f63cd3148005f79f
FER <megatroninlove@gmail.com>
2016-03-25 19:21:44 -0500

    plataforma/profesor/materia
    casi terminado

--- view/plataforma/profesor/materia/la-materia.php
+++ view/plataforma/profesor/materia/la-materia.php
@@ -1,1 +1,213 @@
-<img src="resources/img/temporal/g1.jpg">
+<div class="contEstandar bWhite p20 ng-scope">
+
+    <div class="col-sm-4 col-xs-4 bVerde4 fv_pc1" style="min-height:415px;">
+        <div class="p10">
+        
+            <div class="colorfff t16 ff2 tU">Próxima Clase</div>
+            <hr>
+            <div class="col-sm-3 col-xs-3">
+                <img src="resources/img/icons/fv_clnd.png">
+            </div>
+            <div class="col-sm-9 col-xs-9">
+                <div class="t20 colorfff ff0 mb5">Miércoles,</div>
+                <div class="t20 colorfff ff0 mb5">Enero 18 / 2016</div>
+                <div class="t16 colorfff ff0 mb5">7:00 am - 8:30 am</div>
+            </div>
+            <br style="clear:both"><br><br>
+            
+            <div class="col-sm-3 col-xs-3 taC">
+                <img src="resources/img/icons/fv_doc.png">
+            </div>
+            <div class="col-sm-9 col-xs-9">
+                <div class="t14 colorfff ff2 mb5">Tema reciente</div>
+                <div class="t16 colorfff ff0 mb5">Multiplicación y división de polinomios</div>
+            </div>
+
+            <br style="clear:both">
+            <a href="" class="bBasic3 rr20 taC dB fv_btn1 fv_btn1a">Crear Actividad</a>
+            
+        </div>
+    </div>
+
+    <div class="col-sm-4 col-xs-4 bGray3 fv_pc1" style="min-height:415px;">
+        <div class="p10">
+        
+            <div class="color333 ff2 t16 tU">Rendimiento por logros</div>
+            <hr style="border-color:#666;">
+            <div class="navBarLog ff5 taC mb5">
+                <!-- Nav tabs -->
+                <ul class="nav nav-tabs" role="tablist">
+                    <li role="presentation" class="taC active fv_rpl1b">
+                        <a href="#p1" target="_self" aria-controls="p1" role="tab" data-toggle="tab">
+                            <b>(1)P</b>
+                        </a>
+                    </li>
+                    <li role="presentation" class="taC fv_rpl1b">
+                        <a href="#p2" target="_self" aria-controls="p2" role="tab" data-toggle="tab">
+                            <b>(2)P</b>
+                        </a>
+                    </li>
+                    <li role="presentation" class="taC fv_rpl1b">
+                        <a href="#p3" target="_self" aria-controls="p3" role="tab" data-toggle="tab">
+                            <b>(3)P</b>
+                        </a>
+                    </li>
+        
+                </ul>
+            </div>
+            
+            <div class="tab-content">
+            <div id="p1" class="tab-pane fade in active">
+                <br>
+                <div class="contBar mb5 fv_rpl">
+                <div class="fv_rpl1 p10 colorfff ff2" style="width:30%;">1.1</div>
+                    <span class="p5 colorfff ff2 porceFul fv_rpl1a">30%</span>
+                </div>
+                <div class="contBar mb5 fv_rpl">
+                <div class="fv_rpl1 p10 colorfff ff2" style="width:45%;">1.2</div>
+                    <span class="p5 colorfff ff2 porceFul fv_rpl1a">45%</span>
+                </div>
+                <div class="contBar mb5 fv_rpl">
+                <div class="fv_rpl1 p10 colorfff ff2" style="width:65%;">1.3</div>
+                    <span class="p5 colorfff ff2 porceFul fv_rpl1a">65%</span>
+                </div>
+                <div class="contBar mb5 fv_rpl">
+                <div class="fv_rpl1 p10 colorfff ff2" style="width:38%;">1.4</div>
+                    <span class="p5 colorfff ff2 porceFul fv_rpl1a">38%</span>
+                </div>
+            </div>
+            <div id="p2" class="tab-pane fade in">
+                <br>
+                <div class="contBar mb5 fv_rpl">
+                <div class="fv_rpl1 p10 colorfff ff2" style="width:30%;">1.1</div>
+                    <span class="p5 colorfff ff2 porceFul fv_rpl1a">30%</span>
+                </div>
+                <div class="contBar mb5 fv_rpl">
+                <div class="fv_rpl1 p10 colorfff ff2" style="width:45%;">1.2</div>
+                    <span class="p5 colorfff ff2 porceFul fv_rpl1a">45%</span>
+                </div>
+                <div class="contBar mb5 fv_rpl">
+                <div class="fv_rpl1 p10 colorfff ff2" style="width:65%;">1.3</div>
+                    <span class="p5 colorfff ff2 porceFul fv_rpl1a">65%</span>
+                </div>
+                <div class="contBar mb5 fv_rpl">
+                <div class="fv_rpl1 p10 colorfff ff2" style="width:38%;">1.4</div>
+                    <span class="p5 colorfff ff2 porceFul fv_rpl1a">38%</span>
+                </div>
+            </div>
+            <div id="p3" class="tab-pane fade in">
+                <br>
+                <div class="contBar mb5 fv_rpl">
+                <div class="fv_rpl1 p10 colorfff ff2" style="width:30%;">1.1</div>
+                    <span class="p5 colorfff ff2 porceFul fv_rpl1a">30%</span>
+                </div>
+                <div class="contBar mb5 fv_rpl">
+                <div class="fv_rpl1 p10 colorfff ff2" style="width:45%;">1.2</div>
+                    <span class="p5 colorfff ff2 porceFul fv_rpl1a">45%</span>
+                </div>
+                <div class="contBar mb5 fv_rpl">
+                <div class="fv_rpl1 p10 colorfff ff2" style="width:65%;">1.3</div>
+                    <span class="p5 colorfff ff2 porceFul fv_rpl1a">65%</span>
+                </div>
+                <div class="contBar mb5 fv_rpl">
+                <div class="fv_rpl1 p10 colorfff ff2" style="width:38%;">1.4</div>
+                    <span class="p5 colorfff ff2 porceFul fv_rpl1a">38%</span>
+                </div>
+            </div>
+
+            </div>
+
+            <a href="" class="bBasic3 rr20 taC dB fv_btn1 fv_btn1b">Ver Plan de Estudio</a>           
+            <br style="clear:both">
+            
+        </div>
+    </div>
+
+<div class="col-sm-4 col-xs-4 bGray fv_pc1" style="min-height:415px;">
+<div class="p10">
+
+<div class="color333 ff2 t16 tU">Promedio</div>
+<hr style="border-color:#666;">
+<div class="color333 ff2 t18">Matemáticas</div>
+            
+<div class="colorBlack t40 ff2 fR">3.8</div>
+
+<div class="fv_promedio">
+<div class="fv_promedio_t">Promedio</div>
+<div class="fv_prom_cont mb5 rr0">
+<div class="fv_gris1 colorfff ff2" style="width:60%;">
+    <div class="fv_verde1 p10 colorfff ff2" style="width:40%;"></div>
+</div>
+<span class="color000 porceFul">40%</span>
+</div>
+</div>
+
+<div class="fv_per colorVirde ff2 tU t14">        
+<div class="row">
+    <div class="col-sm-2 col-xs-2">(1)P</div>
+    <div class="col-sm-2 col-xs-2">(2)P</div>
+    <div class="col-sm-2 col-xs-2">(3)P</div>
+    <div class="col-sm-2 col-xs-2">EF</div>
+</div> 
+</div>
+<div class="row ff2 t16">
+    <div class="col-sm-2 col-xs-2 vcenter"><span class="ff0 t12">3.6</span></div>
+    <div class="col-sm-2 col-xs-2 vcenter"><span class="ff0 t12">4.2</span></div>
+    <div class="col-sm-2 col-xs-2 vcenter"><img src="resources/img/icons/block.png"></div>
+    <div class="col-sm-2 col-xs-2 vcenter"><img src="resources/img/icons/block.png"></div>
+</div>
+
+<a href="" class="bBasic3 rr20 taC dB fv_btn1 fv_btn1c">Ingresar Nota</a>
+<br style="clear:both">
+
+</div>
+</div>
+    
+<br style="clear:both"><br>
+
+<div class="titSep colorVirde ff2 tU t16"><img src="resources/img/icons/adm3.png"> Ausencias sin justificar</div>
+    
+    <div class="table-responsive">
+        <table class="table table-hover">
+            <tbody>
+                          <tr>
+                <td class="taC"><img src="resources/img/demo/not1.png" class="rr50" width="48"></td>
+                <td><a href="#/plataforma/admin/estudiante" class="aSS">Felipe Cortés Bejarano</a></td>
+                <td>Lunes, Ene 13 / 2016</td>
+                <td>&nbsp;</td>
+                <td class="taC"><a href="" class="bBasic2 rr20 colorfff bVerde taC">Justificar</a></td>
+              </tr>
+                          <tr>
+                <td class="taC"><img src="resources/img/demo/not1.png" class="rr50" width="48"></td>
+                <td><a href="#/plataforma/admin/estudiante" class="aSS">Felipe Cortés Bejarano</a></td>
+                <td>Lunes, Ene 13 / 2016</td>
+                <td>&nbsp;</td>
+                <td class="taC"><a href="" class="bBasic2 rr20 colorfff bVerde taC">Justificar</a></td>
+              </tr>
+                          <tr>
+                <td class="taC"><img src="resources/img/demo/not1.png" class="rr50" width="48"></td>
+                <td><a href="#/plataforma/admin/estudiante" class="aSS">Felipe Cortés Bejarano</a></td>
+                <td>Lunes, Ene 13 / 2016</td>
+                <td>&nbsp;</td>
+                <td class="taC"><a href="" class="bBasic2 rr20 colorfff bVerde taC">Justificar</a></td>
+              </tr>
+                          <tr>
+                <td class="taC"><img src="resources/img/demo/not1.png" class="rr50" width="48"></td>
+                <td><a href="#/plataforma/admin/estudiante" class="aSS">Felipe Cortés Bejarano</a></td>
+                <td>Lunes, Ene 13 / 2016</td>
+                <td>&nbsp;</td>
+                <td class="taC"><a href="" class="bBasic2 rr20 colorfff bVerde taC">Justificar</a></td>
+              </tr>
+                          <tr>
+                <td class="taC"><img src="resources/img/demo/not1.png" class="rr50" width="48"></td>
+                <td><a href="#/plataforma/admin/estudiante" class="aSS">Felipe Cortés Bejarano</a></td>
+                <td>Lunes, Ene 13 / 2016</td>
+                <td>&nbsp;</td>
+                <td class="taC"><a href="" class="bBasic2 rr20 colorfff bVerde taC">Justificar</a></td>
+              </tr>
+                        </tbody>
+        </table>
+    </div>
+
+
+</div>
