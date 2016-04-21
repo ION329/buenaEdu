7cdd43aa71eb79d2318b88c8b1c5f7170fd5b5a2
ION329 <advanced.ion@gmail.com>
2016-04-21 14:07:50 -0500

    LineTime

--- view/plataforma/estudiante/perfil/perfil.php
+++ view/plataforma/estudiante/perfil/perfil.php
@@ -1,132 +1,129 @@
 
-        <!--<div ng-include src=" 'view/plataforma/estudiante/perfil/perfil.php' "></div>-->
-        <!--<img src="resources/img/temporal/images/e-perfil_01.jpg">-->
-	<div class="p20">	
-		<div class="contEstandar bWhite rr5 p10 mb10">
-			<div class="row m0 mb15">
-				<div class="col-xs-4 col-sm-4">
-					<span class="t16 colorVirde ff2 dIB">CLASES DE HOY</span>&nbsp;
-					<select class="selecStd1 bS1 rr5 ff1 t16 bGray">
-						<option value="grado-6">Hoy</option>
-						<option value="grado-7">Matematicas</option>
-						<option value="grado-8">Ciencias</option>
-						<option value="grado-9">Musica</option>
-						<option value="grado-10">Quimica</option>
-						<option value="grado-11">Fisica</option>
-					</select>&nbsp;
-					<div class="bGray5 p510 rr10 dIB colorfff ff2">5</div>
-				</div>
-				<div class="col-xs-2 col-sm-2 col-xs-offset-6 col-sm-offset-6 taR">
-					<a class="bBasic3 rr40 t14 color000 taC cP bCambiar p510 bWhite ff1 dIB">Ver horario</a>
-				</div>			
-			</div>
+<div class="p20">	
+    <div class="contEstandar bWhite rr5 p10 mb10">
+        <div class="row m0 mb15">
+            <div class="col-xs-4 col-sm-4">
+                <span class="t16 colorVirde ff2 dIB">CLASES DE HOY</span>&nbsp;
+                <select class="selecStd1 bS1 rr5 ff1 t16 bGray">
+                    <option value="grado-6">Hoy</option>
+                    <option value="grado-7">Matematicas</option>
+                    <option value="grado-8">Ciencias</option>
+                    <option value="grado-9">Musica</option>
+                    <option value="grado-10">Quimica</option>
+                    <option value="grado-11">Fisica</option>
+                </select>&nbsp;
+                <div class="bGray5 p510 rr10 dIB colorfff ff2">5</div>
+            </div>
+            <div class="col-xs-2 col-sm-2 col-xs-offset-6 col-sm-offset-6 taR">
+                <a class="bBasic3 rr40 t14 color000 taC cP bCambiar p510 bWhite ff1 dIB">Ver horario</a>
+            </div>			
+        </div>
 
-			<div class="row m0">
-				<img src="resources/img/temporal/linea-tiempo.png">
-			</div>
-		</div>
+        <div ng-include src=" 'view/plataforma/zoom/linetime.php' "></div>
+        
+    </div>
 
-		<div class="row m0">
-			<div class="col-xs-8 col-sm-8" style="padding-left: 0px">
-				<div class="contEstandar bWhite rr5">
-					<div class="tab p10" style="border-bottom: 1px solid #ccc;">
-						<div class="tabIn t16 colorVirde ff2">IMPORTANTE</div>
-						<div class="tabIn taR"><a class="bBasic3 rr40 t14 color000 taC cP bCambiar p510 bWhite ff1 dIB">VER MAS NOTIFICACIONES</a></div>
-					</div>
+    <div class="row m0">
+        <div class="col-xs-8 col-sm-8" style="padding-left: 0px">
+            <div class="contEstandar bWhite rr5">
+                <div class="tab p10" style="border-bottom: 1px solid #ccc;">
+                    <div class="tabIn t16 colorVirde ff2">IMPORTANTE</div>
+                    <div class="tabIn taR"><a class="bBasic3 rr40 t14 color000 taC cP bCambiar p510 bWhite ff1 dIB">VER MAS NOTIFICACIONES</a></div>
+                </div>
 
-					
-					<div class="p10">
-						<div class="row m0 p20 bGray3 mb5 cP">
-							<div class="col-xs-2 col-sm-2 p0 taC">
-								<img src="resources/img/temporal/icono.gif">
-							</div><!--
+                
+                <div class="p10">
+                    <div class="row m0 p20 bGray3 mb5 cP">
+                        <div class="col-xs-2 col-sm-2 p0 taC">
+                            <img src="resources/img/temporal/icono.gif">
+                        </div><!--
 
-						--><div class="col-xs-8 col-sm-8 p0 vcenter">
-								<div class="mb5	"><strong>Metodos y tecnicas matematicas</strong></div><br>
-								<div class="color999 mb10">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis odit beatae, consequatur adipisci quisquam minima atque, illo, impedit hic obcaecati.</div>
-								<div class="colorVirde dIB">Por:</div><div class="aSS dIB">Belizario Marin</div>
-							</div><!--
+                    --><div class="col-xs-8 col-sm-8 p0 vcenter">
+                            <div class="mb5	"><strong>Metodos y tecnicas matematicas</strong></div><br>
+                            <div class="color999 mb10">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis odit beatae, consequatur adipisci quisquam minima atque, illo, impedit hic obcaecati.</div>
+                            <div class="colorVirde dIB">Por:</div><div class="aSS dIB">Belizario Marin</div>
+                        </div><!--
 
-						--><div class="col-xs-2 col-sm-2 bGray3 p0 taR vcenter">
-								<img src="resources/img/icons/arL.png">
-							</div><!--
-					--></div>
+                    --><div class="col-xs-2 col-sm-2 bGray3 p0 taR vcenter">
+                            <img src="resources/img/icons/arL.png">
+                        </div><!--
+                --></div>
 
-						<div class="row m0 p20 bGray3 mb5">
-							<div class="col-xs-2 col-sm-2 p0 taC">
-								<img src="resources/img/icons/notif2.png">
-							</div><!--
+                    <div class="row m0 p20 bGray3 mb5">
+                        <div class="col-xs-2 col-sm-2 p0 taC">
+                            <img src="resources/img/icons/notif2.png">
+                        </div><!--
 
-						--><div class="col-xs-5 col-sm-5 p0 vcenter">
-								<div class="mb5	"><strong>Taller de matematicas</strong></div><br>
-								<div class="color999 dIB">Subido por:</div><div class="aSS dIB">Pepito Perez</div><br>
-								<div class="color999">Fecha: 10/02/16</div>
-								
-							</div><!--
+                    --><div class="col-xs-5 col-sm-5 p0 vcenter">
+                            <div class="mb5	"><strong>Taller de matematicas</strong></div><br>
+                            <div class="color999 dIB">Subido por:</div><div class="aSS dIB">Pepito Perez</div><br>
+                            <div class="color999">Fecha: 10/02/16</div>
+                            
+                        </div><!--
 
-						--><div class="col-xs-5 col-sm-5 bGray3 p0 taR vcenter">
-								<a class="bBasic3 rr40 colorfff bVerde5 ff2" href="">Descargar Archivo</a>
-							</div><!--
-					--></div>
+                    --><div class="col-xs-5 col-sm-5 bGray3 p0 taR vcenter">
+                            <a class="bBasic3 rr40 colorfff bVerde5 ff2" href="">Descargar Archivo</a>
+                        </div><!--
+                --></div>
 
-						<div class="row m0 p20 bGray3 mb5 cP">
-							<div class="col-xs-2 col-sm-2 p0 taC">
-								<img src="resources/img/temporal/icono.gif">
-							</div><!--
+                    <div class="row m0 p20 bGray3 mb5 cP">
+                        <div class="col-xs-2 col-sm-2 p0 taC">
+                            <img src="resources/img/temporal/icono.gif">
+                        </div><!--
 
-						--><div class="col-xs-8 col-sm-8 p0 vcenter">
-								<div class="aSS dIB">Campamento departamental Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore voluptatibus, quam.</div>
-							</div><!--
+                    --><div class="col-xs-8 col-sm-8 p0 vcenter">
+                            <div class="aSS dIB">Campamento departamental Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore voluptatibus, quam.</div>
+                        </div><!--
 
-						--><div class="col-xs-2 col-sm-2 bGray3 p0 taR vcenter">
-								<img src="resources/img/icons/arL.png">
-							</div><!--
-					--></div>
+                    --><div class="col-xs-2 col-sm-2 bGray3 p0 taR vcenter">
+                            <img src="resources/img/icons/arL.png">
+                        </div><!--
+                --></div>
 
 
-						
-					</div>
-				</div>
-			</div>
+                    
+                </div>
+            </div>
+        </div>
 
-			<div class="col-xs-4 col-sm-4 p0" style="padding-right: 0px">
-				<div class="contEstandar bWhite rr5 p0">
+        <div class="col-xs-4 col-sm-4 p0" style="padding-right: 0px">
+            <div class="contEstandar bWhite rr5 p0">
 
-					<div class="tab p10" style="border-bottom: 1px solid #ccc;">
-						<div class="tabIn t16 colorVirde ff2" style="padding: 4px">PROFESORES RECIENTES</div>
-					</div>
+                <div class="tab p10" style="border-bottom: 1px solid #ccc;">
+                    <div class="tabIn t16 colorVirde ff2" style="padding: 4px">PROFESORES RECIENTES</div>
+                </div>
 
-					<?php for($rr=1; $rr<5; $rr++){ ?>
+                <?php for($rr=1; $rr<5; $rr++){ ?>
 
-					<div class="row m0 p10">
+                <div class="row m0 p10">
 
-						<div class="col-xs-2 col-sm-2 p0 vcenter">
-							<img src="resources/img/demo/user2.png">
-						</div><!--
+                    <div class="col-xs-2 col-sm-2 p0 vcenter">
+                        <img src="resources/img/demo/user2.png">
+                    </div><!--
 
-					--><div class="col-xs-6 col-sm-6 vcenter">
-							<div class="colorVirde dB mb5"><strong>Juanito Perez Cortes</strong></div>
-							<div class="color999 dB">Matematicas</div>
-						</div><!--
+                --><div class="col-xs-6 col-sm-6 vcenter">
+                        <div class="colorVirde dB mb5"><strong>Juanito Perez Cortes</strong></div>
+                        <div class="color999 dB">Matematicas</div>
+                    </div><!--
 
-					--><div class="col-xs-4 col-sm-4 p0 vcenter taR">
-							<a class="bBasic3 rr40 colorfff bVerde5 ff2" href="">Mensaje</a>
-						</div><!--
+                --><div class="col-xs-4 col-sm-4 p0 vcenter taR">
+                        <a class="bBasic3 rr40 colorfff bVerde5 ff2" href="">Mensaje</a>
+                    </div><!--
 
-				--></div>
+            --></div>
 
-					<?php } ?>
+                <?php } ?>
 
-					<div class="row m0">
-						<div class="col-xs-12 col-sm-12 p20 cP">
-							<div class="color999 dIB">Ver todos los profesores</div>&nbsp;&nbsp;&nbsp;&nbsp;
-							<img src="resources/img/icons/arL.png">
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-	</div>	
+                <div class="row m0">
+                    <div class="col-xs-12 col-sm-12 p20 cP">
+                        <div class="color999 dIB">Ver todos los profesores</div>&nbsp;&nbsp;&nbsp;&nbsp;
+                        <img src="resources/img/icons/arL.png">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>	
 
 
 <div ng-include src=" 'view/plataforma/general/area/footer.php' "></div>
---
+++ view/plataforma/zoom/linetime.php
@@ -0,0 +1,88 @@
+<div class="row m0 mb20 bS1">
+    <div class="col-sm-12 col-xs-12 p0">
+        <div class="bloqLineTime bGray">
+        	<center>
+                <div class="tab" style="width:1120px; position:relative;">
+                    <!--
+                        widht: horas totales de la jornada * 4 * 35 
+                    
+                    -->
+                    <?php for($tt=0; $tt<32; $tt++){ ?>                    
+                        <div class="tabIn bloqLineTimeIn taC color333 bGray3"></div>                        
+                    <?php } ?>
+                    
+                    
+                    <!-- MATERIA EN HORARIO -->
+            
+                            <div class="tabAll materiaEst bSR BSL" style="left:0px; width:210px;" ><!-- WIDTH = cada 30 mins * 24px ;  TOP = #horaInicial - 6 * 24 + #horas - 6 -->
+                                <div class="tabIn">
+                                    <div class="taC color333 colorHH ff2 mb30">Matemáticas</div>
+                                    <div class="taC colorVerde3 colorHH t40 mb10">4.8</div>
+                                    <div class="taC color333 mb30 colorHH"><span class="p310 bS1 rr20 cP">ver notas</span></div>
+                                    <div class="taC color333 ff2 colorHH"><img src="resources/img/icons/ok.png"> Asistencia</div>
+                                    <div class="horaIniciaMateria color666 ff1">8:00</div>
+                                    <div class="horaFinalMateria color666 ff1">9:30</div>
+                                </div>
+                            </div>
+                            
+                            <div class="tabAll materiaEst materiactual bSR BSL" style="left:280px; width:280px;" ><!-- WIDTH = cada 30 mins * 24px ;  TOP = #horaInicial - 6 * 24 + #horas - 6 -->
+                                <div class="tabIn">
+                                    <div class="taC color333 color ff2 mb30 colorHH">Lengua y literatura</div>
+                                    <div class="taC colorOrange t40 mb10 colorHH">3.2</div>
+                                    <div class="taC color333 mb30 colorHH"><span class="p310 bS1 rr20 cP">ver notas</span></div>
+                                    <div class="taC color333 ff2 colorHH">En curso</div>
+                                    <div class="horaIniciaMateria color666 ff1">10:00</div>
+                                    <div class="horaFinalMateria color666 ff1">12:00</div>
+                                </div>
+                            </div>
+                            <div class="tabAll materiaEst bSR BSL" style="left:560px; width:140px;" ><!-- WIDTH = cada 30 mins * 24px ;  TOP = #horaInicial - 6 * 24 + #horas - 6 -->
+                                <div class="tabIn">
+                                    <div class="taC color333 colorHH ff2 mb30">Biología</div>
+                                    <div class="taC colorRed colorHH t40 mb10">2.9</div>
+                                    <div class="taC color333 mb30 colorHH"><span class="p310 bS1 rr20 cP">ver notas</span></div>
+                                    <div class="taC color333 ff2 colorHH">Muy pronto</div>
+                                    <div class="horaIniciaMateria color666 ff1">12:00</div>
+                                    <div class="horaFinalMateria color666 ff1">13:00</div>
+                                </div>
+                            </div>
+                            <div class="tabAll materiaEst bSR BSL" style="left:770px; width:210px;" ><!-- WIDTH = cada 30 mins * 24px ;  TOP = #horaInicial - 6 * 24 + #horas - 6 -->
+                                <div class="tabIn">
+                                    <div class="taC color333 colorHH ff2 mb30">Formación y valores</div>
+                                    <div class="taC colorOrange colorHH t40 mb10">3.2</div>
+                                    <div class="taC color333 mb30 colorHH"><span class="p310 bS1 rr20 cP">ver notas</span></div>
+                                    <div class="taC color333 ff2 colorHH">Muy pronto</div>
+                                    <div class="horaIniciaMateria color666 ff1">13:30</div>
+                                    <div class="horaFinalMateria color666 ff1">15:00</div>
+                                </div>
+                            </div>
+                            <div class="tabAll materiaEst bSR BSL" style="left:980px; width:140px;" ><!-- WIDTH = cada 30 mins * 24px ;  TOP = #horaInicial - 6 * 24 + #horas - 6 -->
+                                <div class="tabIn">
+                                    <div class="taC color333 colorHH ff2 mb30">Ingles</div>
+                                    <div class="taC colorVerde3 colorHH t40 mb10">4.4</div>
+                                    <div class="taC color333 mb30 colorHH"><span class="p310 bS1 rr20 cP">ver notas</span></div>
+                                    <div class="taC color333 ff2 colorHH">Muy pronto</div>
+                                    <div class="horaIniciaMateria color666 ff1">15:00</div>
+                                    <div class="horaFinalMateria color666 ff1">16:00</div>
+                                </div>
+                            </div>
+                            
+                    <!-- // MATERIA EN HORARIO // -->
+                    
+                    
+                    <!-- HORA ACTUAL -->
+                        <div class="lineTime2" style="left:380px;"></div>
+                    <!-- // HORA ACTUAL -->
+                    
+                        
+                    
+                    
+                </div>
+            </center>
+            <br><br>
+        </div>
+    </div>
+    
+    
+    
+    
+</div>
